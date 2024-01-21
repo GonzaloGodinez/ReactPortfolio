fa27ba05cc90ac967bad32aecb000071b50e2825
GonzaloGodinez <Gonzalo.Godinez@comcast.net>
2024-01-21 01:47:02 -0700

    add Home,About pages & nav and Navbar jsx files

---
+++ vite-project/src/pages/AboutPage.jsx
@@ -0,0 +1,60 @@
+// This is a static page mocking an "About Us" section for our fake user data
+export default function AboutPage() {
+  return (
+    <div className="container pt-4">
+      <p>
+        Quality Control Analyst
+
+        Quality Control Analyst with experience quickly identifying quality problems and recommending solutions. Strengths in identifying and troubleshooting problems. Key skills include:
+
+        •	Technical: Extensive experience in Operating Systems IBM Mainframe Complexes |IBM OS | Microsoft products | Windows | MVS | VM/CMS | UNIX.
+        •	Vast Experience in Languages and Programming Tools such as COBOL | Power Builder | Visual Basic | DCL | CA 7 | TSO/ISPF | DCL | VMS | CList | Syncsort | FileAid | SPUFI | QMF | Xpediter | Endeavor | Query search.
+        •	Great experience with Database and Data Communications DB2 | SQL-DC/DS | Oracle | VSAM | CICS |
+        Power Builder | IDMS | Oracle | IMS | JCL.
+      </p>
+      <section className="features-icons bg-light text-center m-4">
+        <div className="container">
+          <div className="row p-2">
+            <div className="col-lg-4">
+              <div className="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+                <div className="features-icons-icon d-flex">
+                  <i className="bi-window m-auto text-primary" />
+                </div>
+                <h3>Fully Responsive</h3>
+                <p className="lead mb-0">
+                  This theme will look great on any device, no matter the size!
+                </p>
+              </div>
+            </div>
+            <div className="col-lg-4">
+              <div className="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+                <div className="features-icons-icon d-flex">
+                  <i className="bi-layers m-auto text-primary" />
+                </div>
+                <h3>Bootstrap 5 Ready</h3>
+                <p className="lead mb-0">
+                  Featuring the latest build of the new Bootstrap 5 framework!
+                </p>
+              </div>
+            </div>
+            <div className="col-lg-4">
+              <div className="features-icons-item mx-auto mb-0 mb-lg-3">
+                <div className="features-icons-icon d-flex">
+                  <i className="bi-terminal m-auto text-primary" />
+                </div>
+                <h3>Easy to Use</h3>
+                <p className="lead mb-0">
+                  Ready to use with your own content, or customize the source
+                  files!
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <p>
+        I am a very analytical, disciplined and committed person that respects peers, upper-level executive management and has respect for job deadlines. I have vast experience analyzing data at a higher level and have a robust IT background, and I am continuing learning and upgrading my coding skills. Currently, I am taking HTML and CSS full stack web development classes at Denver University to upgrade my programming developing skills. I work well as an individual and as part of a team that will be responsible for accomplishing complex jobs.
+      </p>
+    </div>
+  );
+}
---
+++ vite-project/src/pages/HomePage.jsx
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react';
+// Bringing in the required component from 'react-router-dom' for linking between pages
+
+export default function HomePage() {
+  // Prior to the return statement, our homepage uses a few react hooks and fetchData function to query to a mock database and retrieve random user data
+
+
+  // Iterate over each mock user to display their abridged profile data and a link to their page
+  return (
+    <div className="container pt-4">
+      enter hero banner,
+       picture or 
+       intro
+Quality Control Analyst
+
+Quality Control Analyst with experience quickly identifying quality problems and recommending solutions. Strengths in identifying and troubleshooting problems. Key skills include:
+
+•	Technical: Extensive experience in Operating Systems IBM Mainframe Complexes |IBM OS | Microsoft products | Windows | MVS | VM/CMS | UNIX.
+•	Vast Experience in Languages and Programming Tools such as COBOL | Power Builder | Visual Basic | DCL | CA 7 | TSO/ISPF | DCL | VMS | CList | Syncsort | FileAid | SPUFI | QMF | Xpediter | Endeavor | Query search.
+•	Great experience with Database and Data Communications DB2 | SQL-DC/DS | Oracle | VSAM | CICS |
+Power Builder | IDMS | Oracle | IMS | JCL.
+    </div>
+  );
+}
