510707ec277773745268e6a08482aa15cdfc887c
GonzaloGodinez <Gonzalo.Godinez@comcast.net>
2024-01-30 01:27:24 -0700

    Updated Home, About, Project, Contact, ResumePage.jsx files

--- vite-project/src/pages/ContactPage.jsx
+++ vite-project/src/pages/ContactPage.jsx
@@ -4,57 +4,76 @@ import { useState, useEffect } from 'react';
 export default function ContactPage() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('')
+  const [validname, setValidName] = useState('')
+  const [validemail, setValidEmail] = useState('')
+
 
   useEffect(() => {
-  // Validate name
-  if (name === '') {
-    console.log('Name is required');
-  }
-    // validate email
-    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
-      console.log('Email is invalid')
+    // Validate name
+    if (name === '') {
+      setValidName('Name is required');
     }
-  }, [name, email]);
+    else{setValidName("Name is valid")}
+  }, [name]);
+
+  useEffect(() => {
+    // validate email
+    if (email === "") {
+      setValidEmail("Email is required");
+    } else
+      if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+        setValidEmail('Email is invalid');
+      }else{setValidEmail("Email is valid")}
+  }, [email]);
 
   // data entry for name, email and message
-  function handleEmail() {
+  function handleEmail(e) {
+    e.preventDefault()
     const floatingName = document.getElementById("floatingName").value
     const floatingEmail = document.getElementById("floatingEmail").value
     const floatingMessage = document.getElementById("floatingMessage").value
-    window.location.href = `mailto:Gonzalo.Godinez@comcast.net?subject=${floatingName}&body=${floatingMessage}`
+    // window.location.href = `mailto:Gonzalo.Godinez@comcast.net?subject=${floatingName}&body=${floatingMessage}`
   }
 
   // created a form
   return (
     <div className="container pt-4">
-      <p>
-      </p>
-      <div class="form-floating my-3">
-        <textarea class="form-control" placeholder="Leave a comment here" id="floatingName" value={name} onChange={(e) => setName(e.target.value)}></textarea>
+      <form className="row g-3" onSubmit={handleEmail}>
+      <div className="form-floating my-3">
+        <input type="text" required className="form-control" placeholder="Leave a comment here" id="floatingName" value={name} onChange={(e) => setName(e.target.value)} />
         <label for="floatingName">Name</label>
+
       </div>
-      <div class="form-floating my-3">
-        <textarea class="form-control" placeholder="Leave a comment here" id="floatingEmail" value={email} onChange={(e) => setEmail(e.target.value)}></textarea>
+        <div className= "">
+          {validname}
+        </div>
+      <div className="form-floating my-3">
+        <input type="email" required className="form-control" placeholder="Leave a comment here" id="floatingEmail" value={email} onChange={(e) => setEmail(e.target.value)} />
         <label for="floatingEmail">Email</label>
       </div>
-      <div class="form-floating my-2">
-        <textarea class="form-control" placeholder="Leave a comment here" id="floatingMessage"></textarea>
+        <div className="">
+        {validemail}
+        </div>
+      <div className="form-floating my-2">
+        <textarea className="form-control" required placeholder="Leave a comment here" id="floatingMessage" value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
         <label for="floatingMessage">Message</label>
       </div>
-      <button type="button" onClick={handleEmail} class="btn btn-success w-100">Send</button>
+      <button type="submit" className="btn btn-success w-100">Send</button>
+      </form>
 
       <footer>
-        <a href="https://github.com/GonzaloGodinez" target="_blank" className="btn btn-primary me-3"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
+        <a href="https://github.com/GonzaloGodinez" target="_blank" className="btn btn-primary me-3"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-github" viewBox="0 0 16 16">
           <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8" />
         </svg></a>
 
         <a href="https://www.facebook.com/gonzalo.godinez.3/" target="_blank"
-          className="btn btn-primary me-3"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
+          className="btn btn-primary me-3"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-facebook" viewBox="0 0 16 16">
             <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951" />
           </svg></a>
 
         <a href="https://www.linkedin.com/in/gonzalo-godinez-78163627a/?lipi=urn%3Ali%3Apage%3Ad_flagship3_feed%3B2KDFE3HVTICt7hjnGvrCOw%3D%3D" target="_blank"
-          className="btn btn-primary me-3"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
+          className="btn btn-primary me-3"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-linkedin" viewBox="0 0 16 16">
             <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z" />
           </svg></a>
         <h2>Made with ❤️️ by GGL</h2>
